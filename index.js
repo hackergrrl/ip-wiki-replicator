1f8d34578dabdbd5fea989fb764f6c1c66295b13
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-03-13 15:02:17 -0700

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+var swarm = require('webrtc-swarm')
+var signalhub = require('signalhub')
+
+if (process.argv.length !== 3) {
+  console.error("USAGE: ip-wiki-replicator /ipfs/Qmfoobaripwikiroothash")
+  process.exit(1)
+}
+
+var root = process.argv[2]
+var wiki = 'ip-wiki'
+
+var sw = swarm(signalhub(wiki, ['https://signalhub.mafintosh.com']))
+
+sw.on('peer', function (peer, id) {
+  peer.send(root)
+  console.log('got peer.. wrote!')
+})
+
+console.log('ready')
